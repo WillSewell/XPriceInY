ade45bbef7615de2b67e8683419dd691c62356b2
Will Sewell <me@willsewell.name>
2014-03-23 23:05:55 +0000

    Created a simple base android project. The dependency management is set up with Ivy. Hacked together a very simple initial screen to test it built correctly.

---
+++ src/name/willsewell/XPriceInY/XPriceInY.java
@@ -0,0 +1,15 @@
+package name.willsewell.XPriceInY;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class XPriceInY extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
