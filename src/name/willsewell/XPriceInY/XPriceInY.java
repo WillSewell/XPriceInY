9d16ca5ca517977d8d8163c27eb9bdf2fa2cfa23
Will Sewell <me@willsewell.name>
2014-03-24 22:03:20 +0000

    Added a means to read in app config. Added functionality to read in the CPI data from the CSV.

--- src/name/willsewell/XPriceInY/XPriceInY.java
+++ src/name/willsewell/XPriceInY/XPriceInY.java
@@ -2,8 +2,19 @@ package name.willsewell.XPriceInY;
 
 import android.app.Activity;
 import android.os.Bundle;
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 public class XPriceInY extends Activity {
+
+    private Properties properties;
+    private List cPIList;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +22,18 @@ public class XPriceInY extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        properties = new Properties();
+        try {
+            properties.load(new BufferedReader(new FileReader("config.properties")));
+            cPIList = parseCSV(properties.getProperty("cpi-csv-path"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List parseCSV(String fname) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(fname));
+        return reader.readAll();
     }
 }
