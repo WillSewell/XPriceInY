59726ed9d7d869cdf2c0ea0497523d1371580b11
Will Sewell <me@willsewell.name>
2014-03-25 23:41:45 +0000

    Moved the app config because it could not be loaded correctly from the root directory. The app is now able to read in the CPI CSV and, extract the countries and display them in an android "spinner". A very rudimentary interface has been created for the other GUI elements too.

--- src/name/willsewell/XPriceInY/XPriceInY.java
+++ src/name/willsewell/XPriceInY/XPriceInY.java
@@ -1,19 +1,22 @@
 package name.willsewell.XPriceInY;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import au.com.bytecode.opencsv.CSVReader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class XPriceInY extends Activity {
 
     private Properties properties;
-    private List cPIList;
+    private ArrayList<String[]> cPIList;
 
     /**
      * Called when the activity is first created.
@@ -23,17 +26,27 @@ public class XPriceInY extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // load the app config and load the CPI CSV file
+        AssetManager am = getAssets();
         properties = new Properties();
         try {
-            properties.load(new BufferedReader(new FileReader("config.properties")));
-            cPIList = parseCSV(properties.getProperty("cpi-csv-path"));
+            properties.load(getAssets().open("config.properties"));
+            InputStream is = am.open(properties.getProperty("cpi-csv-path"));
+            CSVReader reader = new CSVReader(new InputStreamReader(is));
+            cPIList = (ArrayList<String[]>) reader.readAll();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    private List parseCSV(String fname) throws IOException {
-        CSVReader reader = new CSVReader(new FileReader(fname));
-        return reader.readAll();
+        // extract just the countries into their own array
+        ArrayList<String> countries = new ArrayList<>();
+        for (String[] item : cPIList) {
+            countries.add(item[0]);
+        }
+
+        // create a new spinner to attach the array of countries to
+        Spinner spinner = (Spinner) findViewById(R.id.home_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, countries);
+        spinner.setAdapter(adapter);
     }
 }
